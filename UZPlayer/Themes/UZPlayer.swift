e6710988df1f285e87df8ddee30252f54afb9a86
Long Nguyen <vietlongn.dev@gmail.com>
2020-07-20 18:05:22 +0700

    @objc loadVideo

--- UZPlayer/Themes/UZPlayer.swift
+++ UZPlayer/Themes/UZPlayer.swift
@@ -351,7 +351,7 @@ open class UZPlayer: UIView {
 	- parameter url: URL of linkplay
 	- parameter subtitleURLs: URLs of subtitle if any
 	*/
-	open func loadVideo(url: URL, subtitleURLS: [URL]? = nil) {
+	@objc open func loadVideo(url: URL, subtitleURLS: [URL]? = nil) {
 		let linkPlay = UZVideoLinkPlay(definition: "", url: url)
 		let item = UZVideoItem(name: "", thumbnailURL: nil, linkPlay: linkPlay, subtitleURLs: subtitleURLS)
 		loadVideo(item)
@@ -362,7 +362,7 @@ open class UZPlayer: UIView {
 	
 	- parameter video: UZVideoItem
 	*/
-	open func loadVideo(_ video: UZVideoItem) {
+	@objc open func loadVideo(_ video: UZVideoItem) {
 		UZLogger.shared.log(event: "loadstart")
 		if currentVideo != nil {
 			stop()
